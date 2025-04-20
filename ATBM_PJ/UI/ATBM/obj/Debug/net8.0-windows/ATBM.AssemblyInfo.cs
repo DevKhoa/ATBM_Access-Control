d429eeb0f7b12806e63adb70d4682864eeee8524
HoangYen2004 <lhy26042k4@gmail.com>
2025-04-20 07:20:51 +0700

    INITIAL 1

--- ATBM_PJ/UI/ATBM/obj/Debug/net8.0-windows/ATBM.AssemblyInfo.cs
+++ ATBM_PJ/UI/ATBM/obj/Debug/net8.0-windows/ATBM.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ATBM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e86761e73dac008681f44a0dfaa62cfd96063653")]
 [assembly: System.Reflection.AssemblyProductAttribute("ATBM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ATBM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
